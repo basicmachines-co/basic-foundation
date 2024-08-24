51d9a156cfdaa0b3a1c2263b78877934fd573980
phernandez <phernandez@basicmachines.co>
2024-08-24 14:07:32 -0500

    refactor flash messages to base templates

--- app/src/foundation/web/routes.py
+++ app/src/foundation/web/routes.py
@@ -5,6 +5,7 @@ from fastapi import Depends, status
 from fastapi import Request, Form, Response, APIRouter
 from fastapi.encoders import jsonable_encoder
 from fastapi.security import OAuth2PasswordRequestForm
+from jinja2 import pass_context
 from jinja2.ext import DebugExtension
 from jinja2_fragments.fastapi import Jinja2Blocks
 from pydantic import ValidationError
@@ -18,7 +19,7 @@ from foundation.users.models import User
 from foundation.users.schemas import AuthTokenPayload, UserCreate, Message
 from foundation.users.services import UserCreateError, UserValueError, UserNotFoundError
 from foundation.web.deps import CurrentUserDep, access_token_security
-from foundation.web.utils import get_flashed_message, flash
+from foundation.web.utils import flash
 
 html_router = APIRouter(include_in_schema=False, default_response_class=HTMLResponse)
 
@@ -26,6 +27,23 @@ templates = Jinja2Blocks(
     directory=f"{BASE_DIR}/templates",
 )
 templates.env.add_extension(DebugExtension)
+
+
+class FlashMessageJinja(Jinja2Blocks):
+    def _create_env(self, *args, **kwargs):
+        env = super()._create_env(*args, **kwargs)
+        env.globals["get_flashed_message"] = self.get_flashed_message
+        return env
+
+    @pass_context
+    def get_flashed_message(self, context):
+        request = context["request"]
+        message = request.session.pop("flash", None)
+        return message
+
+
+# Set up Jinja2 templates with the custom class
+templates = FlashMessageJinja(directory="templates")
 
 
 @html_router.get("/")
@@ -62,13 +80,11 @@ async def users(
     pagination = user_pagination.paginate(request, query, page_size=page_size)
     page = await pagination.page(page=page)
 
-    flash_message = get_flashed_message(request)
     return templates.TemplateResponse(
         "pages/user_list.html",
         {
             "request": request,
             "page": page,
-            "flash_message": flash_message
         },
     )
 
