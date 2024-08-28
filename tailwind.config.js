e3d2c5089d610b72473b610b7bce130c892c0755
phernandez <phernandez@basicmachines.co>
2024-08-28 17:46:21 -0500

    add tailwind font again

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./templates/**/*.html",
     "./static/**/*.js"
   ],
-  plugins: [require("@tailwindcss/forms")]
+  plugins: [require("@tailwindcss/forms")],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans]
+      }
+    }
+  }
 };
