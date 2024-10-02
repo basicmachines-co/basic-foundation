f7c0def6101181e4944d3ad5e972b8e22034393c
phernandez <phernandez@basicmachines.co>
2024-10-02 12:12:28 -0500

    jinjax navbar refactor

--- foundation/web/templates.py
+++ foundation/web/templates.py
@@ -1,5 +1,6 @@
 import typing
 
+import jinjax
 from jinja2.ext import DebugExtension
 from starlette.requests import Request
 from starlette.templating import Jinja2Templates
@@ -8,6 +9,10 @@ from foundation.core.config import BASE_DIR
 
 templates = Jinja2Templates(directory=f"{BASE_DIR}/web/templates")
 templates.env.add_extension(DebugExtension)
+
+templates.env.add_extension(jinjax.JinjaX)  # pyright: ignore [reportPrivateImportUsage]
+catalog = jinjax.Catalog(jinja_env=templates.env)  # pyright: ignore [reportPrivateImportUsage]
+catalog.add_folder(f"{BASE_DIR}/web/components")
 
 
 def template(
