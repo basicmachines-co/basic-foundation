ed62cab4c115e95caac750c886c445155e333351
phernandez <phernandez@basicmachines.co>
2024-10-12 15:57:49 -0500

    add basic-components

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,14 +1,20 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  darkMode: "selector",
+  darkMode: "class",
   content: ["./templates/**/*.html", "./**/*.jinja", "./static/**/*.js"],
-  plugins: [require("@tailwindcss/forms")],
+  plugins: [require("@tailwindcss/forms"), require("tailwindcss-animate")],
   theme: {
     extend: {
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
       fontFamily: {
         sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
+      colors: {},
     },
   },
 };
